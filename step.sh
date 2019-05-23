7ddddb870d8ea72f834460e6283d4982742f70ca
Jeffrey Macko <jmacko@solocal.com>
2019-05-23 17:53:04 +0200

    Add most of the work

--- step.sh
+++ step.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# Compute the number of warnings
+test_name=$(/usr/libexec/PlistBuddy -c "Print :TestableSummaries:0:TestName" ${BITRISE_XCRESULT_PATH}/TestSummaries.plist)
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# Trim all spaces
+test_count=$(/usr/libexec/PlistBuddy -c "Print :TestsCount" ${BITRISE_XCRESULT_PATH}/Info.plist)
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+
+# Test regression
+./cli-regression-protector "test_relapse_${test_name}" "$warning_number" "<" ".ci/ci.sqlite3"
