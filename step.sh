41548b46eaee2065c869c0f61dbdbac3bb903529
Jeffrey Macko <jmacko@solocal.com>
2019-05-28 14:03:57 +0200

    Mise en forme de tout le step

--- step.sh
+++ step.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -ex
 
-# Get the number of warnings
-test_name=$(/usr/libexec/PlistBuddy -c "Print :TestableSummaries:0:TestName" ${BITRISE_XCRESULT_PATH}/TestSummaries.plist)
+TestCount=$(/usr/libexec/PlistBuddy -c "Print :TestsCount" ${BITRISE_XCRESULT_PATH}/Info.plist)
+TestFailedCount=$(/usr/libexec/PlistBuddy -c "Print :TestsFailedCount" ${BITRISE_XCRESULT_PATH}/Info.plist)
+TestErrorCount=$(/usr/libexec/PlistBuddy -c "Print :ErrorCount" ${BITRISE_XCRESULT_PATH}/Info.plist)
+TestWarningCount=$(/usr/libexec/PlistBuddy -c "Print :WarningCount" ${BITRISE_XCRESULT_PATH}/Info.plist)
 
-# Trim all spaces
-test_count=$(/usr/libexec/PlistBuddy -c "Print :TestsCount" ${BITRISE_XCRESULT_PATH}/Info.plist)
+echo "Test Count: $TestCount"
+echo "Test Failed Count: $TestFailedCount"
+echo "Test Error Count: $TestErrorCount"
+echo "Test Warning Count: $TestWarningCount"
 
-
-# Test regression
-./relapse "test_relapse_${test_name}" "$warning_number" ">" ".ci/ci.sqlite3"
+./relapse "TestCount_relapse_$BITRISE_SCHEME" "$TestCount" ">" ".ci/ci.sqlite3"
+./relapse "TestFailedCount_relapse_$BITRISE_SCHEME" "$TestFailedCount" "<" ".ci/ci.sqlite3"
+./relapse "TestErrorCount_relapse_$BITRISE_SCHEME" "$TestErrorCount" "<" ".ci/ci.sqlite3"
+./relapse "TestWarningCount_relapse_$BITRISE_SCHEME" "$TestWarningCount" "<" ".ci/ci.sqlite3"
