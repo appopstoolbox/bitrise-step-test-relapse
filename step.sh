18449e7a11a89550253a44da8912777a9fb35f92
CI <ci-mojo@macko.fr>
2019-05-28 17:39:03 +0200

    Fix some path issue

--- step.sh
+++ step.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 TestCount=$(/usr/libexec/PlistBuddy -c "Print :TestsCount" $xcresultPath/Info.plist)
 TestFailedCount=$(/usr/libexec/PlistBuddy -c "Print :TestsFailedCount" $xcresultPath/Info.plist)
 TestErrorCount=$(/usr/libexec/PlistBuddy -c "Print :ErrorCount" $xcresultPath/Info.plist)
@@ -11,10 +12,10 @@ echo "Test Failed Count: $TestFailedCount"
 echo "Test Error Count: $TestErrorCount"
 echo "Test Warning Count: $TestWarningCount"
 
-./relapse "TestCount_relapse_$BITRISE_SCHEME" "$TestCount" ">" ".ci/ci.sqlite3"
-./relapse "TestFailedCount_relapse_$BITRISE_SCHEME" "$TestFailedCount" "<" ".ci/ci.sqlite3"
-./relapse "TestErrorCount_relapse_$BITRISE_SCHEME" "$TestErrorCount" "<" ".ci/ci.sqlite3"
-./relapse "TestWarningCount_relapse_$BITRISE_SCHEME" "$TestWarningCount" "<" ".ci/ci.sqlite3"
+$SCRIPTPATH/relapse "TestCount_relapse_$BITRISE_SCHEME" "$TestCount" ">" ".ci/ci.sqlite3"
+$SCRIPTPATH/relapse "TestFailedCount_relapse_$BITRISE_SCHEME" "$TestFailedCount" "<" ".ci/ci.sqlite3"
+$SCRIPTPATH/relapse "TestErrorCount_relapse_$BITRISE_SCHEME" "$TestErrorCount" "<" ".ci/ci.sqlite3"
+$SCRIPTPATH/relapse "TestWarningCount_relapse_$BITRISE_SCHEME" "$TestWarningCount" "<" ".ci/ci.sqlite3"
 
 git add .ci/ci.sqlite3
 git commit -am "Update Test Count database"
