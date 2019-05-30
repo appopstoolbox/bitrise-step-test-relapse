c96db4555dfec78a417416bb52dd17370bb273d6
CI <ci-mojo@macko.fr>
2019-05-31 00:33:41 +0200

    Improve implementation NFC

--- step.sh
+++ step.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+DBPath=".ci/ci.sqlite3"
+ScriptPath=$( cd "$(dirname "$0")" ; pwd -P )
 TestCount=$(/usr/libexec/PlistBuddy -c "Print :TestsCount" $xcresultPath/Info.plist)
 TestFailedCount=$(/usr/libexec/PlistBuddy -c "Print :TestsFailedCount" $xcresultPath/Info.plist)
 TestErrorCount=$(/usr/libexec/PlistBuddy -c "Print :ErrorCount" $xcresultPath/Info.plist)
@@ -12,11 +13,11 @@ echo "Test Failed Count: $TestFailedCount"
 echo "Test Error Count: $TestErrorCount"
 echo "Test Warning Count: $TestWarningCount"
 
-$SCRIPTPATH/relapse "TestCount_relapse_$BITRISE_SCHEME" "$TestCount" ">" ".ci/ci.sqlite3"
-$SCRIPTPATH/relapse "TestFailedCount_relapse_$BITRISE_SCHEME" "$TestFailedCount" "<" ".ci/ci.sqlite3"
-$SCRIPTPATH/relapse "TestErrorCount_relapse_$BITRISE_SCHEME" "$TestErrorCount" "<" ".ci/ci.sqlite3"
-$SCRIPTPATH/relapse "TestWarningCount_relapse_$BITRISE_SCHEME" "$TestWarningCount" "<" ".ci/ci.sqlite3"
+$ScriptPath/relapse "TestCount_relapse_$BITRISE_SCHEME" "$TestCount" ">" $DBPath
+$ScriptPath/relapse "TestFailedCount_relapse_$BITRISE_SCHEME" "$TestFailedCount" "<" $DBPath
+$ScriptPath/relapse "TestErrorCount_relapse_$BITRISE_SCHEME" "$TestErrorCount" "<" $DBPath
+$ScriptPath/relapse "TestWarningCount_relapse_$BITRISE_SCHEME" "$TestWarningCount" "<" $DBPath
 
-git add .ci/ci.sqlite3
+git add $DBPath
 git commit -am "Update Test Count database"
 git push origin HEAD:$BITRISE_GIT_BRANCH
